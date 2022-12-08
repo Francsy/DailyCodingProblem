ebf63bec761b92fae2723235949d05e5f7265f60
Francsy <francsyludens@gmail.com>
2022-12-08 18:27:26 +0100

    Second exercise solved

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # DailyCodingProblem
-I´ll try to solve problems from https://www.dailycodingproblem.com/
+
+![imagen](/assets/icon-round.png)
+
+I´ll try to solve coding problems from https://www.dailycodingproblem.com/
+
+Each exercise is in comment blocks within the file with its level of difficulty offered by dailycodeproblem.
--- dailycodingproblem.js
+++ dailycodingproblem.js
@@ -1,5 +1,6 @@
-/* This problem was recently asked by Google.
-#1 Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+/* #1 [Easy] 
+This problem was recently asked by Google.
+Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
 For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 */
 
@@ -14,3 +15,23 @@ function numbersChecker(list, k) {
     }
     return check
 }
+
+/* #2 [Hard]
+This problem was asked by Uber.
+Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
+For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6] 
+*/
+
+function productAllLessSelf(arr) {
+    let newArr = []
+        for (let i = 0; i < arr.length; i++) {
+          let multiplier = 1
+            for (let j = 0; j < arr.length; j++) {
+                if (i != j){
+                    multiplier *= arr[j]
+                }
+            } 
+          newArr.push(multiplier)
+        }
+    return newArr
+    }
