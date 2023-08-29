b39dd7bb6935cf04a17c991a28bdb4a1fcaa3ad6
Francsy <francsyludens@gmail.com>
2023-08-29 20:17:08 +0200

    solve/exercise #3

--- dailycodingproblem.js
+++ dailycodingproblem.js
@@ -1,10 +1,12 @@
 /* #1 [Easy] 
 This problem was recently asked by Google.
+
 Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
 For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 */
 
 // First version (nested loop)
+
 function numbersChecker(list, k) {
     let check = false;
     for (let i = 0; i < list.length; i++) {
@@ -47,6 +49,7 @@ function numbersChecker3V(list, k) {
 
 /* #2 [Hard]
 This problem was asked by Uber.
+
 Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
 For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6] 
 */
@@ -76,6 +79,7 @@ function productAllLessSelfLongVersion(arr) {
 
 /* #3 [Medium]
 This problem was asked by Google.
+
 Given the root to a binary tree, implement serialize(root), which serializes the tree into a string, and deserialize(s), which deserializes the string back into the tree.
 For example, given the following Node class
 
@@ -91,14 +95,47 @@ node = Node('root', Node('left', Node('left.left')), Node('right'))
 assert deserialize(serialize(node)).left.left.val == 'left.left'
  */
 
-class Node {
+// The exercise is written in Python, we will translate it to JS.
+
+class Node { 
     constructor(val, left = null, right = null) {
         this.val = val;
         this.left = left;
         this.right = right;
     }
-
 }
+
+const serialize = root => {
+    if (!root) {
+        return null;
+    }
+    return JSON.stringify({
+        val: root.val,
+        left: serialize(root.left),
+        right: serialize(root.right),
+    })
+}
+
+const deserialize= string => {
+    if (!string) {
+        return null;
+    }
+    const node = JSON.parse(string);
+    const left = deserialize(node.left);
+    const right = deserialize(node.right);
+    return new Node(node.val, left, right);
+}
+
+/* Testing:
+
+const tree = new Node("root", new Node("left", new Node("left.left")), new Node("right"));
+
+const serializedTree = serialize(tree);
+const deserializeTree = deserialize(serializedTree); 
+console.log(deserializeTree.left.left.val); // Output: 'left.left'
+
+*/
+
 
 /* #11 [Medium]
 This problem was asked by Twitter.
