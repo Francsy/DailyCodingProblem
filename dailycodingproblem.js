be2ca2220cda3fefeaefd8f31a4c0cdb7bbfd4b8
Francsy <francsyludens@gmail.com>
2023-08-29 10:48:47 +0200

    reorganize/exercises

--- dailycodingproblem.js
+++ dailycodingproblem.js
@@ -62,7 +62,7 @@ class Node {
 
 }
 
-/* #3 [Medium]
+/* #11 [Medium]
 This problem was asked by Twitter.
 Implement an autocomplete system. That is, given a query string s and a set of all possible query strings, return all strings in the set that have s as a prefix.
 For example, given the query string de and the set of strings [dog, deer, deal], return [deer, deal].
