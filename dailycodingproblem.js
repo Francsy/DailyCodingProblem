4bd2db32dfce05687399a2af9885b04c9a44d8ae
Francsy <francsyludens@gmail.com>
2023-09-24 23:25:37 +0200

    solve/ exercise #12 first part

--- dailycodingproblem.js
+++ dailycodingproblem.js
@@ -325,9 +325,6 @@ const nonAdjacentMaxSum = (arr) => {
 
 
 
-
-
-
 /* #10 [Medium] --------------------------------------------------------------
 
 This problem was asked by Apple.
@@ -382,6 +379,16 @@ For example, if N is 4, then there are 5 unique ways:
 What if, instead of being able to climb 1 or 2 steps at a time, you could climb any number from a set of positive integers X? For example, if X = {1, 3, 5}, you could climb 1, 3, or 5 steps at a time.
 
 */
+
+const countWaysToClimb = (N) => {
+    const dp = new Array(N + 1).fill(0)
+    dp[0] = 1;
+    dp[1] = 1;
+    for (let i = 2; i <= N; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[N];
+}
 
 /* #13 [Hard] --------------------------------------------------------------
 This problem was asked by Amazon.
